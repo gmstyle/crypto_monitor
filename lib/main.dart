dadca2ec4bcf4d0f4d6614578e1b125aee5c721c
Gabriele Martina <gabriele.martina@icsone.it>
2023-05-26 09:44:11 +0200

    dynamic color in favore di flex color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:crypto_monitor/providers/api_provider.dart';
 import 'package:crypto_monitor/repositories/detail_repository.dart';
 import 'package:crypto_monitor/repositories/home_repository.dart';
 import 'package:crypto_monitor/router/app_router.dart';
-import 'package:crypto_monitor/ui/themes/app_themes.dart';
 import 'package:crypto_monitor/utils/my_bloc_observer.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pine/di/dependency_injector_helper.dart';
@@ -31,12 +31,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Crypto Monitor',
-      darkTheme: AppThemes.dark,
-      theme: AppThemes.light,
-      themeMode: ThemeMode.system,
-      routerConfig: appRouter,
-    );
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      ColorScheme lightColorScheme;
+      ColorScheme darkColorScheme;
+
+      if (lightDynamic != null && darkDynamic != null) {
+        lightColorScheme = lightDynamic.harmonized();
+        darkColorScheme = darkDynamic.harmonized();
+      } else {
+        lightColorScheme = const ColorScheme.light(
+          primary: Color(0xFF6200EE),
+        );
+        darkColorScheme = const ColorScheme.dark(
+          primary: Color(0xFF6200EE),
+          brightness: Brightness.dark,
+        );
+      }
+      return MaterialApp.router(
+        title: 'Crypto Monitor',
+        theme: ThemeData(colorScheme: lightColorScheme, useMaterial3: true),
+        darkTheme: ThemeData(colorScheme: darkColorScheme, useMaterial3: true),
+        routerConfig: appRouter,
+      );
+    });
   }
 }
