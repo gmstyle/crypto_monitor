7d797c8acfc968157631592ea0fbfb38a7010543
Gabriele Martina <contattagmstyle@gmail.com>
2023-07-06 10:37:14 +0200

    Aggiornate dipendenze + icona launcher

--- lib/ui/screens/dashboard/tabs/settings/settings_tab.dart
+++ lib/ui/screens/dashboard/tabs/settings/settings_tab.dart
@@ -6,7 +6,7 @@ class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Settings'),
+      child: Text('Settings TODO'),
     );
   }
 }
